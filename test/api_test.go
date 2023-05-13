3168e3ae633a530fb4e7804b01d3199366ed48d7
RRRRomeo <johnneylulu@163.com>
2023-05-13 14:08:59 +0800

    feat(api): add support for std log;

--- test/api_test.go
+++ test/api_test.go
@@ -1,38 +1,10 @@
 package api
 
 import (
-	"sync"
 	"testing"
 
-	"github.com/QLog/internal"
+	"github.com/QLog/api"
 )
-
-func TestLogApi_Debugf(t *testing.T) {
-	type fields struct {
-		Mutex  sync.Mutex
-		logger *internal.Logger
-	}
-	type args struct {
-		fmt string
-		v   []any
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			log := &LogApi{
-				Mutex:  tt.fields.Mutex,
-				logger: tt.fields.logger,
-			}
-			log.Debugf(tt.args.fmt, tt.args.v...)
-		})
-	}
-}
 
 func TestDebugf(t *testing.T) {
 	type args struct {
@@ -47,7 +19,7 @@ func TestDebugf(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			Debugf(tt.args.fmt, tt.args.v...)
+			api.Debugf(tt.args.fmt, tt.args.v...)
 		})
 	}
 }
